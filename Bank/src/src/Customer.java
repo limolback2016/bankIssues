57704c3d702176fb1876df049d7a3bae23ad37a5
Li M <limolback@hotmail.se>
2018-01-05 18:08:00 +0100

    [task] uppgiften är klar
    PS1: Test passes

--- Bank/src/src/Customer.java
+++ Bank/src/src/Customer.java
@@ -1,3 +1,8 @@
+/***
+ * This class contents all informaiton of
+ * a customer and its account's info. 
+ * @author Li Molback, limolb-5
+ */
 package src;
 
 import java.lang.reflect.Array;
@@ -15,45 +20,85 @@ public class Customer {
 		this.idNo = pID;
 	}
 	
+	/**
+	 * Set customer's first name
+	 * @param foreName
+	 */
 	public void setForename(String foreName){
 		this.name = foreName;
 	}
 	
+	/**
+	 * Get customer's first name
+	 * @return customer's first name
+	 */
 	public String getForename(){
 		return this.name;
 	}
 	
+	/**
+	 * Set customer's surname
+	 * @param surName
+	 */
 	public void setSurname(String surName){
 		this.surName = surName;
 	}
 	
+	/**
+	 * Get customer's surname
+	 * @return customer's surname
+	 */
 	public String getSurname(){
 		return this.surName;
 	}
 	
-	// ID no should not be changed while it sets
+	/**
+	 * Get customer's ID
+	 * @return customer's ID 
+	 */
 	public String getIdNo(){
 		return this.idNo;
 	}
 	
-	// add account
+	/**
+	 * create an new account for the customer 
+	 * @param s add an account to the customer
+	 */
 	public void addAccount(SavingsAccount s){
 		accounts.add(s);
 	}
 	
-	// get account
+	/**
+	 * Get all accounts of a customer
+	 * @return a list of the customer's accounts 
+	 */
 	public List<SavingsAccount> getAccounts(){
 		return accounts;
 	}
 	
+	/**
+	 * Get account for a customer 
+	 * with right ID number 
+	 * @param id the customer ID with connecting to the account
+	 * @return accounts which connected with a customer ID
+	 */
 	public SavingsAccount getAccount(int id){
 		return accounts.get(id);
 	}
 	
+	/**
+	 * print customer's information such as name and ID no. 
+	 * @return customer name and ID no
+	 */
 	public String getCustomerInfo() {
 		return getForename() + " " +  getSurname() + " " + getIdNo();
 	}
 	
+	/**
+	 * check if an account does exist or not 
+	 * @param accountNumber which is checked
+	 * @return check against account list
+	 */
 	public int findAccount(int accountNumber){
 		for (int i = 0 ; i < accounts.size() ; i++){
 			if ( accounts.get(i).getAccountNumber() == accountNumber){
@@ -61,6 +106,14 @@ public class Customer {
 			}
 		}
 			return -1;
+	}
+	
+	/**
+	 * check if an account does exist and then delete it 
+	 * @param accountNumber which is checked
+	 */
+	public void closeAccount(int accountNumber) {
+		accounts.remove(findAccount(accountNumber));
 	}
 	
 }
